9c49c6b3af5554e67f9b08103c882fbbc41d0fa0
EHronek <emmanuelhronek@gmail.com>
2025-05-23 09:55:05 +0300

    logging database queries

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import sqlite3
+import functools
+
+
+def log_queries(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        query = kwargs.get('query', args[0] if args else None)
+
+        # Log query
+        print(f"Executing query: {query}")
+
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+
+users = fetch_all_users(query="SELECT * FROM users")
