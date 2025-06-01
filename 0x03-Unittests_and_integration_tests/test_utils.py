65d756cc4dbd02df685f7f51e14c316c4ae09545
EHronek <emmanuelhronek@gmail.com>
2025-06-01 11:47:15 +0300

    Parameterize a unittest linter error fix

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,8 +16,8 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """Test that the method returns what it is supposed to"""
         self.assertEqual(access_nested_map(nested_map=nested_map, path=path),
-                        expected)
-        
+                         expected)
+
 
 if __name__ == "__main__":
     unittest.main()
