34db91eb19f046a63c6b03a814cd71cc7dcf6350
EHronek <emmanuelhronek@gmail.com>
2025-06-08 18:52:26 +0300

    detect and block offensive language

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -39,3 +39,35 @@ class RestrictAccessByTimeMiddleware:
         ):
             return HttpResponseForbidden("Access to the messaging app is restricted between 9PM and 6AM.")
         return self.get_response(request)
+
+
+class OffensiveLanguageMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.message_counts = {}
+        self.rate_limit = 5
+        self.time_window = 60
+
+    def __call__(self, request):
+        if request.method == "POST" and request.path == '/api/chats/messages':
+            ip_address = self.get_client_ip(request)
+            current_time = time()
+            if ip_address not in self.message_counts:
+                self.message_counts[ip_address] = []
+            self.message_counts[ip_address] = [
+                t for t in self.message_counts[ip_address] if current_time -t < self.time_window
+            ]
+            if len(self.message_counts[ip_address]) >= self.rate_limit:
+                return HttpResponseForbidden("Message limit exceeded: 5 per minute")
+            self.message_counts[ip_address].append(current_time)
+        response = self.get_response(request)
+        return response
+    
+    def get_client_ip(self, request):
+        """Safely extract client IP address"""
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
