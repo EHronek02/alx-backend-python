deea543ca4b85806dcce71b7f0da4c30b36bbef1
Emmanuel Hronek <emmanuelhronek@gmail.com>
2025-11-22 13:07:51 +0300

    created django messaging app

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -2,7 +2,7 @@
 URL configuration for messaging_app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,21 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Include API URLs under /api/ path
-    path('api/', include('chats.urls')),
-    
-    # Add API auth URLs as required by checker
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # login
-    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh') # refresh
 ]
