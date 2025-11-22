6b56e3a715b4259d1d9b7214e9d454ec7120ae03
Emmanuel Hronek <emmanuelhronek@gmail.com>
2025-11-22 22:29:32 +0300

    api views

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from . import views
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
+
 router.register(r'users', views.UserViewSet)
 router.register(r'conversations', views.ConversationViewSet)
 router.register(r'messages', views.MessageViewSet)
